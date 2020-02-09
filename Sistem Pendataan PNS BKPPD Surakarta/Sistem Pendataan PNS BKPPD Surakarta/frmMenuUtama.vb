1b55b97bbb67daa4592232b3131e955e0d839284
AlfandyZein <46378303+AlfandyZein@users.noreply.github.com>
2020-02-09 22:07:46 +0700

    Add files via upload

---
+++ Sistem Pendataan PNS BKPPD Surakarta/Sistem Pendataan PNS BKPPD Surakarta/frmMenuUtama.vb
@@ -0,0 +1,10 @@
+﻿Public Class frmMenuUtama
+
+    Private Sub btnDaftarBaru_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnDaftarBaru.Click
+        frmDaftar.Show()
+    End Sub
+
+    Private Sub btnUpdate_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnUpdate.Click
+        frmUpdate.Show()
+    End Sub
+End Class
